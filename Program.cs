947fb923a0a1bfdeef47d65cefe886c07ddd8c74
Hype <123018649+HypeCrazed@users.noreply.github.com>
2024-05-06 09:31:19 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,4 +4,4 @@ string[] coins = { "Tails", "Heads" };
 var flipChance = new Random();
 string flipResult = coins[flipChance.Next(coins.Length)];
 Console.WriteLine(flipResult);
-Console.ReadKey();
+Console.ReadKey(); // I will probably add an option to flip again but idk
